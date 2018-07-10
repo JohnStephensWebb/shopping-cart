db9185a9afb9d2eab44f6c06d2af441b38bcdfe9
John Stephens-Webb <JohnStephensWebb@Johns-MacBook-Pro-2.local>
2018-07-10 14:28:06 -0600

    Items added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import CartItems from './CartItems'
 
 class App extends Component {
   render() {
+    const CartItemsList = [
+  { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+  { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+  { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+]
     return (
       <div>
       <Header/>
-      <CartItems/>
+      <CartItems items = {CartItemsList}/>
       <CartFooter copyright = '2016'/>
       </div>
     )
--- src/CartItems.js
+++ src/CartItems.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
-const CartItems = () => {
+import CartItem from "./CartItem"
+const CartItems = (props) => {
 return(
   <div className="container">
- <h1>Cart Items</h1>
+  <h1>Cart Items</h1>
+  <div className="list-group">
+    <div className="list-group-item">
+      <div className="row">
+        <div className="col-md-8">Product</div>
+        <div className="col-md-2">Price</div>
+        <div className="col-md-2">Quantity</div>
+      </div>
+    </div>
+    {props.items.map(item => <CartItem key={item.id}
+      price={item.product.priceInCents} quantity={item.quantity} name={item.product.name}/>
+      )}
+  </div>
 </div>
 )
 }
 export default CartItems
+// <div className="container">
+// <h1>Cart Items</h1>
+// <div className="list-group">
+//   <div className="list-group-item">
+//     <div className="row">
+//       <div className="col-md-8">Product</div>
+//       <div className="col-md-2">Price</div>
+//       <div className="col-md-2">Quantity</div>
+//     </div>
+//   </div>
+//   {props.items.map(item => <CartItem key={item.id} price={item.product.priceInCents} quantity={item.product.name} />) }
+// </div>
+// </div>
